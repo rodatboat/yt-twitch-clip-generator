84ca43ab1090ebd2aeb1767a122d3166ea30c937
Ronaldo Suarez <ronaldo3055@gmail.com>
2022-12-12 13:17:51 -0500

    fix

--- driver.py
+++ driver.py
@@ -127,7 +127,8 @@ game_id = [515025]
 
 # game_count = 20, cpg = 5, ld = 7 weekly
 # gc = 1, cpg = 100, ld = 1 daily
-allClips = gen_clips_list(game_count=20, clip_per_game=5, last_days=1, byGame=True, game_id=game_id)
+# allClips = gen_clips_list(game_count=20, clip_per_game=5, last_days=1, byGame=True, game_id=game_id)
+allClips = gen_clips_list(game_count=20, clip_per_game=5, last_days=1)
 dl_clips(allClips)
 filter_clips(maxDuration=max_duration)
 mark_all_clips()
